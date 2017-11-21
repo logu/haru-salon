dd18087fb6d7f59c559de8e2dbfe39346846d069
Logu Kirou <Logu@MacBook-Air-de-Logu.local>
2017-11-21 06:19:44 +0100

    added socket management

--- display/public/src/js/index.js
+++ display/public/src/js/index.js
@@ -42,3 +42,51 @@ function co2Up(count)
         }
     }, int_speed);
 }
+
+function co2Down(count)
+{
+    var div_by = 10,
+        speed = Math.round(count / div_by),
+        $display = $('.air-quality .mesure-value .value'),
+        run_count = 1,
+        int_speed = 200,
+        init_value = parseInt($display.text());
+
+    var int = setInterval(function() {
+        if(run_count > div_by){
+            var result = init_value - (speed * run_count);
+            $display.text(result);
+            $('.c').toggleClass('h',!result >= 800)
+            $('.c').toggleClass('sad', result > 800);
+            run_count--;
+        } else if(parseInt($display.text()) > count) {
+            var curr_count = parseInt($display.text()) - 1;
+            $display.text(curr_count);
+        } else {
+            clearInterval(int);
+        }
+    }, int_speed);
+}
+
+var socket = io(window.location.origin);
+socket.on('cmd', function(msg) {
+    var $Airdisplay = $('.air-quality .mesure-value .value');
+    var $Hotdisplay = $('.temperature .mesure-value .value');
+    console.log(msg);
+    if(msg === 'HOT') {
+        $Hotdisplay.text('23');
+        $('.c').removeClass('h');
+        $('.c').addClass('sad');
+    }
+    if(msg === 'AIR') {
+        $Hotdisplay.text('1000');
+        $('.c').removeClass('h');
+        $('.c').addClass('sad');
+    }
+    if(msg === 'OK') {
+        $Hotdisplay.text('20');
+        $Airdisplay.text('600');
+        $('.c').removeClass('h');
+        $('.c').addClass('sad');
+    }
+});
